c6b6ff67ff769b93ac1ba99de04447d63f1d3b2f
Oğuz Kağan ER <oguzkaganerr@gmail.com>
2022-10-19 14:22:01 +0300

    FormApp_5 : Database bağlantısı kurularak CRUD işlemleri yapabilen basit uygulama.

---
+++ FormApp_5/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace FormApp_5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btn_ekle = new System.Windows.Forms.Button();
+            this.btn_guncelle = new System.Windows.Forms.Button();
+            this.btn_sil = new System.Windows.Forms.Button();
+            this.txt_id = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_user = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_password = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txt_name = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(255, 25);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(441, 229);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // btn_ekle
+            // 
+            this.btn_ekle.Location = new System.Drawing.Point(32, 152);
+            this.btn_ekle.Name = "btn_ekle";
+            this.btn_ekle.Size = new System.Drawing.Size(208, 23);
+            this.btn_ekle.TabIndex = 1;
+            this.btn_ekle.Text = "EKLE";
+            this.btn_ekle.UseVisualStyleBackColor = true;
+            this.btn_ekle.Click += new System.EventHandler(this.btn_ekle_Click);
+            // 
+            // btn_guncelle
+            // 
+            this.btn_guncelle.Location = new System.Drawing.Point(32, 190);
+            this.btn_guncelle.Name = "btn_guncelle";
+            this.btn_guncelle.Size = new System.Drawing.Size(208, 23);
+            this.btn_guncelle.TabIndex = 2;
+            this.btn_guncelle.Text = "GÜNCELLE";
+            this.btn_guncelle.UseVisualStyleBackColor = true;
+            this.btn_guncelle.Click += new System.EventHandler(this.btn_guncelle_Click);
+            // 
+            // btn_sil
+            // 
+            this.btn_sil.Location = new System.Drawing.Point(32, 231);
+            this.btn_sil.Name = "btn_sil";
+            this.btn_sil.Size = new System.Drawing.Size(208, 23);
+            this.btn_sil.TabIndex = 3;
+            this.btn_sil.Text = "SİL";
+            this.btn_sil.UseVisualStyleBackColor = true;
+            this.btn_sil.Click += new System.EventHandler(this.btn_sil_Click);
+            // 
+            // txt_id
+            // 
+            this.txt_id.Location = new System.Drawing.Point(114, 25);
+            this.txt_id.Name = "txt_id";
+            this.txt_id.Size = new System.Drawing.Size(126, 20);
+            this.txt_id.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(22, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Id :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "User :";
+            // 
+            // txt_user
+            // 
+            this.txt_user.Location = new System.Drawing.Point(114, 51);
+            this.txt_user.Name = "txt_user";
+            this.txt_user.Size = new System.Drawing.Size(126, 20);
+            this.txt_user.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(29, 80);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Password :";
+            // 
+            // txt_password
+            // 
+            this.txt_password.Location = new System.Drawing.Point(114, 77);
+            this.txt_password.Name = "txt_password";
+            this.txt_password.Size = new System.Drawing.Size(126, 20);
+            this.txt_password.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(29, 106);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Name :";
+            // 
+            // txt_name
+            // 
+            this.txt_name.Location = new System.Drawing.Point(114, 103);
+            this.txt_name.Name = "txt_name";
+            this.txt_name.Size = new System.Drawing.Size(126, 20);
+            this.txt_name.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(708, 280);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txt_name);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txt_password);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txt_user);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_id);
+            this.Controls.Add(this.btn_sil);
+            this.Controls.Add(this.btn_guncelle);
+            this.Controls.Add(this.btn_ekle);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btn_ekle;
+        private System.Windows.Forms.Button btn_guncelle;
+        private System.Windows.Forms.Button btn_sil;
+        private System.Windows.Forms.TextBox txt_id;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_user;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_password;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txt_name;
+    }
+}
+
---
+++ FormApp_5/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormApp_5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection sqlConnection;
+        SqlCommand sqlCommand;
+        SqlDataAdapter sqlDataAdapter;
+        DataSet dataSet;
+
+        void gridDoldur()
+        {
+            sqlConnection = new SqlConnection("Data Source=403-08;Initial Catalog=dbLogin;User ID=sa;Password=1234");
+            sqlDataAdapter = new SqlDataAdapter("Select * From tblUser", sqlConnection);
+            dataSet = new DataSet();
+            sqlConnection.Open();
+            sqlDataAdapter.Fill(dataSet, "tblUser");
+            dataGridView1.DataSource = dataSet.Tables["tblUser"];
+            sqlConnection.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            gridDoldur();
+        }
+
+        private void btn_ekle_Click(object sender, EventArgs e)
+        {
+            sqlCommand = new SqlCommand();
+            sqlConnection.Open();
+            sqlCommand.Connection = sqlConnection;
+            sqlCommand.CommandText = "insert into tblUser(id,usr,pwd,name) values ('" + txt_id.Text + "','" + txt_user.Text + "','" + txt_password.Text + "','" + txt_name.Text + "')";
+            sqlCommand.ExecuteNonQuery();
+            sqlConnection.Close();
+            gridDoldur();
+        }
+
+        private void btn_guncelle_Click(object sender, EventArgs e)
+        {
+            sqlCommand = new SqlCommand();
+            sqlConnection.Open();
+            sqlCommand.Connection = sqlConnection;
+            sqlCommand.CommandText = "update tblUser set usr='" + txt_user.Text + "',pwd='" + txt_password.Text + "',name='" + txt_name.Text + "' where id=" + txt_id.Text + "";
+            sqlCommand.ExecuteNonQuery();
+            sqlConnection.Close();
+            gridDoldur();
+        }
+
+        private void btn_sil_Click(object sender, EventArgs e)
+        {
+            sqlCommand = new SqlCommand();
+            sqlConnection.Open();
+            sqlCommand.Connection = sqlConnection;
+            sqlCommand.CommandText = "delete from tblUser where id=" + txt_id.Text;
+            sqlCommand.ExecuteNonQuery();
+            sqlConnection.Close();
+            gridDoldur();
+        }
+    }
+}
