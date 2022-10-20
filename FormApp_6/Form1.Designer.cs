8aec9e5b1b8763d991c64ce45d34797b73af37d6
Oğuz Kağan ER <oguzkaganerr@gmail.com>
2022-10-20 09:21:15 +0300

    Yıl-Ay-Gün olacak şekilde yaş hesaplama uygulaması.

---
+++ FormApp_6/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace FormApp_6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.tbl_dogumTarihi = new System.Windows.Forms.Label();
+            this.lbl_yas = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(15, 21);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(302, 20);
+            this.dateTimePicker1.TabIndex = 0;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // tbl_dogumTarihi
+            // 
+            this.tbl_dogumTarihi.AutoSize = true;
+            this.tbl_dogumTarihi.Location = new System.Drawing.Point(94, 59);
+            this.tbl_dogumTarihi.Name = "tbl_dogumTarihi";
+            this.tbl_dogumTarihi.Size = new System.Drawing.Size(0, 13);
+            this.tbl_dogumTarihi.TabIndex = 1;
+            // 
+            // lbl_yas
+            // 
+            this.lbl_yas.AutoSize = true;
+            this.lbl_yas.Location = new System.Drawing.Point(94, 90);
+            this.lbl_yas.Name = "lbl_yas";
+            this.lbl_yas.Size = new System.Drawing.Size(0, 13);
+            this.lbl_yas.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 59);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Doğum Tarihi :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(57, 90);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Yaş :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(329, 127);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lbl_yas);
+            this.Controls.Add(this.tbl_dogumTarihi);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label tbl_dogumTarihi;
+        private System.Windows.Forms.Label lbl_yas;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ FormApp_6/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormApp_6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public int[] CalculateAge(DateTime dateNow, DateTime birthDate)
+        {
+            int day1, month1, year1;
+            int day2, month2, year2;
+            int diffYear, diffMonth, diffDay;
+            diffYear = 0; diffMonth = 0; diffDay = 0;
+            year1 = birthDate.Year;
+            month1 = birthDate.Month;
+            day1 = birthDate.Day;
+            day2 = dateNow.Day;
+            month2 = dateNow.Month;
+            year2 = dateNow.Year;
+            if (day2 < day1)
+            {
+                day2 += DateTime.DaysInMonth(year2, month2);
+                diffDay = day2 - day1;
+                month2--;
+                if (month2 < month1)
+                {
+                    month2 += 12;
+                    year2--;
+                    diffMonth = month2 - month1;
+                    diffYear = year2 - year1;
+                }
+                else
+                {
+                    diffMonth = month2 - month1;
+                    diffYear = year2 - year1;
+                }
+            }
+            else
+            {
+                diffDay = day2 - day1;
+                if (month2 < month1)
+                {
+                    month2 += 12;
+                    year2--;
+                    diffMonth = month2 - month1;
+                    diffYear = year2 - year1;
+                }
+                else
+                {
+                    diffMonth = month2 - month1;
+                    diffYear = year2 - year1;
+                }
+            }
+            int[] sonuc = new int[3];
+            sonuc[0] = diffYear;
+            sonuc[1] = diffMonth;
+            sonuc[2] = diffDay;
+            return sonuc;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            tbl_dogumTarihi.Text = DateTime.Now.ToShortDateString();
+        }
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            int[] result = CalculateAge(DateTime.Now, Convert.ToDateTime(dateTimePicker1.Text));
+            lbl_yas.Text = result[0].ToString() + " Yıl " + result[1].ToString() + " Ay " + result[2].ToString() + " Gün ";
+        }
+    }
+}
