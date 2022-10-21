be2a4f1b7a088cdc54c89cb88aa1d44dd71a0d20
Oğuz Kağan ER <oguzkaganerr@gmail.com>
2022-10-21 14:14:20 +0300

    Dört işlem hesap makinesi

---
+++ FormApp_8/Form1.Designer.cs
@@ -0,0 +1,340 @@
+﻿namespace FormApp_8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lbl_ekran = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.btn_virgul = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
+            this.btn_temizle = new System.Windows.Forms.Button();
+            this.btn_sil = new System.Windows.Forms.Button();
+            this.button17 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbl_ekran
+            // 
+            this.lbl_ekran.BackColor = System.Drawing.Color.White;
+            this.lbl_ekran.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lbl_ekran.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_ekran.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.lbl_ekran.Location = new System.Drawing.Point(32, 24);
+            this.lbl_ekran.Name = "lbl_ekran";
+            this.lbl_ekran.Size = new System.Drawing.Size(182, 39);
+            this.lbl_ekran.TabIndex = 0;
+            this.lbl_ekran.Text = "0";
+            this.lbl_ekran.TextAlign = System.Drawing.ContentAlignment.BottomRight;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Bahnschrift Condensed", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(32, 214);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(41, 40);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "1";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.btn_sayi_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Font = new System.Drawing.Font("Bahnschrift Condensed", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(79, 214);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(41, 40);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "2";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.btn_sayi_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button3.Font = new System.Drawing.Font("Bahnschrift Condensed", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(126, 214);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(41, 40);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "3";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.btn_sayi_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button4.Font = new System.Drawing.Font("Bahnschrift Condensed", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(32, 260);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(88, 40);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "0";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.btn_sayi_Click);
+            // 
+            // btn_virgul
+            // 
+            this.btn_virgul.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btn_virgul.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn_virgul.Font = new System.Drawing.Font("Bahnschrift Condensed", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_virgul.Location = new System.Drawing.Point(126, 260);
+            this.btn_virgul.Name = "btn_virgul";
+            this.btn_virgul.Size = new System.Drawing.Size(41, 40);
+            this.btn_virgul.TabIndex = 5;
+            this.btn_virgul.Text = ",";
+            this.btn_virgul.UseVisualStyleBackColor = false;
+            this.btn_virgul.Click += new System.EventHandler(this.btn_virgul_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button6.Font = new System.Drawing.Font("Bahnschrift Condensed", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.Location = new System.Drawing.Point(32, 168);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(41, 40);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "4";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.btn_sayi_Click);
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button7.Font = new System.Drawing.Font("Bahnschrift Condensed", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button7.Location = new System.Drawing.Point(79, 168);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(41, 40);
+            this.button7.TabIndex = 7;
+            this.button7.Text = "5";
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.btn_sayi_Click);
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button8.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button8.Font = new System.Drawing.Font("Bahnschrift Condensed", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button8.Location = new System.Drawing.Point(126, 168);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(41, 40);
+            this.button8.TabIndex = 8;
+            this.button8.Text = "6";
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.btn_sayi_Click);
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button9.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button9.Font = new System.Drawing.Font("Bahnschrift Condensed", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button9.Location = new System.Drawing.Point(126, 122);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(41, 40);
+            this.button9.TabIndex = 11;
+            this.button9.Text = "9";
+            this.button9.UseVisualStyleBackColor = false;
+            this.button9.Click += new System.EventHandler(this.btn_sayi_Click);
+            // 
+            // button10
+            // 
+            this.button10.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button10.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button10.Font = new System.Drawing.Font("Bahnschrift Condensed", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button10.Location = new System.Drawing.Point(79, 122);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(41, 40);
+            this.button10.TabIndex = 10;
+            this.button10.Text = "8";
+            this.button10.UseVisualStyleBackColor = false;
+            this.button10.Click += new System.EventHandler(this.btn_sayi_Click);
+            // 
+            // button11
+            // 
+            this.button11.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button11.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button11.Font = new System.Drawing.Font("Bahnschrift Condensed", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button11.Location = new System.Drawing.Point(32, 122);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(41, 40);
+            this.button11.TabIndex = 9;
+            this.button11.Text = "7";
+            this.button11.UseVisualStyleBackColor = false;
+            this.button11.Click += new System.EventHandler(this.btn_sayi_Click);
+            // 
+            // button12
+            // 
+            this.button12.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button12.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button12.Font = new System.Drawing.Font("Bahnschrift Condensed", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button12.Location = new System.Drawing.Point(173, 122);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(41, 40);
+            this.button12.TabIndex = 12;
+            this.button12.Text = "x";
+            this.button12.UseVisualStyleBackColor = false;
+            this.button12.Click += new System.EventHandler(this.btn_islem_Click);
+            // 
+            // button13
+            // 
+            this.button13.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button13.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button13.Font = new System.Drawing.Font("Bahnschrift Condensed", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button13.Location = new System.Drawing.Point(173, 168);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(41, 40);
+            this.button13.TabIndex = 13;
+            this.button13.Text = "-";
+            this.button13.UseVisualStyleBackColor = false;
+            this.button13.Click += new System.EventHandler(this.btn_islem_Click);
+            // 
+            // button14
+            // 
+            this.button14.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button14.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button14.Font = new System.Drawing.Font("Bahnschrift Condensed", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button14.Location = new System.Drawing.Point(173, 214);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(41, 86);
+            this.button14.TabIndex = 14;
+            this.button14.Text = "+";
+            this.button14.UseVisualStyleBackColor = false;
+            this.button14.Click += new System.EventHandler(this.btn_islem_Click);
+            // 
+            // btn_temizle
+            // 
+            this.btn_temizle.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btn_temizle.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn_temizle.Font = new System.Drawing.Font("Bahnschrift Condensed", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_temizle.Location = new System.Drawing.Point(79, 76);
+            this.btn_temizle.Name = "btn_temizle";
+            this.btn_temizle.Size = new System.Drawing.Size(88, 40);
+            this.btn_temizle.TabIndex = 15;
+            this.btn_temizle.Text = "C";
+            this.btn_temizle.UseVisualStyleBackColor = false;
+            this.btn_temizle.Click += new System.EventHandler(this.btn_temizle_Click);
+            // 
+            // btn_sil
+            // 
+            this.btn_sil.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btn_sil.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn_sil.Font = new System.Drawing.Font("Bahnschrift Condensed", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_sil.Location = new System.Drawing.Point(32, 76);
+            this.btn_sil.Name = "btn_sil";
+            this.btn_sil.Size = new System.Drawing.Size(41, 40);
+            this.btn_sil.TabIndex = 16;
+            this.btn_sil.Text = "<";
+            this.btn_sil.UseVisualStyleBackColor = false;
+            this.btn_sil.Click += new System.EventHandler(this.btn_sil_Click);
+            // 
+            // button17
+            // 
+            this.button17.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button17.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button17.Font = new System.Drawing.Font("Bahnschrift Condensed", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button17.Location = new System.Drawing.Point(173, 76);
+            this.button17.Name = "button17";
+            this.button17.Size = new System.Drawing.Size(41, 40);
+            this.button17.TabIndex = 17;
+            this.button17.Text = "/";
+            this.button17.UseVisualStyleBackColor = false;
+            this.button17.Click += new System.EventHandler(this.btn_islem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.ClientSize = new System.Drawing.Size(243, 321);
+            this.Controls.Add(this.button17);
+            this.Controls.Add(this.btn_sil);
+            this.Controls.Add(this.btn_temizle);
+            this.Controls.Add(this.button14);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.btn_virgul);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lbl_ekran);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Calculater";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_ekran;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button btn_virgul;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.Button btn_temizle;
+        private System.Windows.Forms.Button btn_sil;
+        private System.Windows.Forms.Button button17;
+    }
+}
+
---
+++ FormApp_8/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormApp_8
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_sayi_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+
+            if (lbl_ekran.Text == "0" || durum == true)
+            {
+                lbl_ekran.Text = btn.Text;
+                if (durum == true)
+                {
+                    durum = false;
+                }
+            }
+            else
+            {
+                lbl_ekran.Text += btn.Text;
+            }
+        }
+        double sonuc = 0;
+        bool durum = false;
+        private void btn_islem_Click(object sender, EventArgs e)
+        {
+            if (durum == false)
+            {
+                durum = true;
+                Button btn = sender as Button;
+                switch (btn.Text)
+                {
+                    case "+":
+                        sonuc += Convert.ToDouble(lbl_ekran.Text);
+                        lbl_ekran.Text = sonuc.ToString();
+                        break;
+                    case "-":
+                        if (sonuc == 0)
+                        {
+                            sonuc = Convert.ToDouble(lbl_ekran.Text);
+                        }
+                        else if (sonuc != 0)
+                        {
+                            sonuc -= Convert.ToDouble(lbl_ekran.Text);
+                            lbl_ekran.Text = sonuc.ToString();
+                        }
+                        break;
+                    case "x":
+                        if (sonuc == 0)
+                        {
+                            sonuc = Convert.ToDouble(lbl_ekran.Text);
+                        }
+                        else if (sonuc != 0)
+                        {
+                            sonuc *= Convert.ToDouble(lbl_ekran.Text);
+                            lbl_ekran.Text = sonuc.ToString();
+                        }
+                        break;
+                    case "/":
+                        if (sonuc == 0)
+                        {
+                            sonuc = Convert.ToDouble(lbl_ekran.Text);
+                        }
+                        else if (sonuc != 0)
+                        {
+                            sonuc /= Convert.ToDouble(lbl_ekran.Text);
+                            lbl_ekran.Text = sonuc.ToString();
+                        }
+                        break;
+                }
+            }
+        }
+
+        private void btn_temizle_Click(object sender, EventArgs e)
+        {
+            sonuc = 0;
+            durum = false;
+            if (lbl_ekran.Text != "0")
+            {
+                lbl_ekran.Text = "0";
+            }
+        }
+
+        private void btn_sil_Click(object sender, EventArgs e)
+        {
+            if (lbl_ekran.Text != "0")
+            {
+                lbl_ekran.Text = lbl_ekran.Text.Remove(lbl_ekran.Text.Length - 1);
+                if (lbl_ekran.Text == string.Empty)
+                {
+                    lbl_ekran.Text = "0";
+                }
+            }
+        }
+
+        private void btn_virgul_Click(object sender, EventArgs e)
+        {
+            if (lbl_ekran.Text.Contains(",") == false)
+            {
+                lbl_ekran.Text += ",";
+            }
+        }
+    }
+}
